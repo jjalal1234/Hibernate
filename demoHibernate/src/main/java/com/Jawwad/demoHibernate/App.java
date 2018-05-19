6731208cd9d5fd5dc1a83d23c089edd5fb6346a9
dfwmtc18 <dfwmtc@gmail.com>
2018-05-18 19:19:06 -0500

    First Hibernate project

--- demoHibernate/src/main/java/com/Jawwad/demoHibernate/App.java
+++ demoHibernate/src/main/java/com/Jawwad/demoHibernate/App.java
@@ -1,13 +1,31 @@
 package com.Jawwad.demoHibernate;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	employees emp = new employees();
+    	emp.setId(4);
+    	emp.setLast_name("Williams");
+    	emp.setFirst_name("David");
+    	emp.setEmail("David.Williams@foo.com");
+    	emp.setDepartment("Engineering");
+    	emp.setSalary(85000);
+    	
+    	Configuration config = new Configuration().configure().addAnnotatedClass(employees.class);
+    	ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(config.getProperties()).buildServiceRegistry();
+    	SessionFactory mySf = config.buildSessionFactory(reg);
+    	Session session = mySf.openSession();
+    	Transaction Tx = session.beginTransaction();
+    	session.save(emp);    	
+    	Tx.commit();
+    	System.out.println(emp);
     }
 }
