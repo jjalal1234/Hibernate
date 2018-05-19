7d2b06bec50aeee7f7b5a051e174e6893288829f
dfwmtc18 <dfwmtc@gmail.com>
2018-05-19 09:01:01 -0500

    Base classes created for Mapping Tables

--- relMapHib/src/main/java/com/Jawwad/relMapHib/App.java
+++ relMapHib/src/main/java/com/Jawwad/relMapHib/App.java
@@ -1,13 +1,36 @@
 package com.Jawwad.relMapHib;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Laptop lapTop = new Laptop();
+        lapTop.setLid(1);
+        lapTop.setLname("Dell");
+        
+    	employees emp = new employees();
+    	emp.setId(1);
+    	emp.setLast_name("Queue");
+    	emp.setFirst_name("Susan");
+    	emp.setEmail("Susan.Queue@foo.com");
+    	emp.setDepartment("Engineering");
+    	emp.setSalary(90000);
+    	
+    	Configuration config = new Configuration().configure().addAnnotatedClass(employees.class).addAnnotatedClass(Laptop.class);
+    	ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(config.getProperties()).buildServiceRegistry();
+    	SessionFactory mySf = config.buildSessionFactory(reg);
+    	Session session = mySf.openSession();
+    	Transaction Tx = session.beginTransaction();
+    	session.save(lapTop);
+    	session.save(emp);    	
+    	Tx.commit();
+    	System.out.println(emp);
     }
 }
---
+++ relMapHib/src/main/java/com/Jawwad/relMapHib/employees.java
@@ -0,0 +1,57 @@
+package com.Jawwad.relMapHib;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class employees {
+	@Id
+	private int id;
+	private String last_name;
+	private String first_name;
+	private String email;
+	private String department;
+	private double salary;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "employees [id=" + id + ", last_name=" + last_name + ", first_name=" + first_name + ", email=" + email
+				+ ", department=" + department + ", salary=" + salary + "]";
+	}
+}
